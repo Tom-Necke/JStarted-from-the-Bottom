47878526152e7e1e1ba44af4ea34b5888c5628dd
Tom Necke <tom.necke@dci-student.de>
2022-04-03 19:54:54 +0200

    started

---
+++ JavaScript-Basics_003/index.js
@@ -0,0 +1,39 @@
+// Check if the following numbers are even numbers. Use a ternary and if the number is even print 30 is even, else print that it is odd.
+// (33, 40, 2, 1, 22)
+
+console.log(33 % 2 == 0 ? "33 is even" : 33 + " " + "is odd");
+console.log(40 % 2 == 0 ? "40 is even" : 40 + " " + "is odd");
+console.log(2 % 2 == 0 ? "2 is even" : 2 + " " + "is odd");
+console.log(1 % 2 == 0 ? "1 is even" : 1 + " " + "is odd");
+console.log(22 % 2 == 0 ? "22 is even" : 22 + " " + "is odd");
+
+let altOpt = 44; 
+
+console.log(altOpt % 2 == 0 ? `${altOpt} is even` : `${altOpt} is odd`);
+
+// Declare a variable named isDog. If true, print 'pat, pat' and if not, print 'find me a dog to pat!'
+
+let isDog = true;
+
+console.log(isDog == true ? `pat, pat` : `find me a dog to pat!`);
+
+// Declare a variable named speedCheck. If speedlimit is 50km/h, and your speed is above that, print 'you're going too fast!'. If speed is lower than 50km/h, print 'You're driving below the speed limit, Oma'.
+
+let speedCheck = 30;
+
+console.log(speedCheck > 50 ? `you're going too fast!` : `You're driving below the speed limit, Oma`);
+
+// Declare a variable named isStudent. If true, print "Ticket costs €5,00". If false, print "Ticket costs €12,00".
+
+let isStudent = true;
+
+console.log(isStudent == true ? "ticket costs €5,00" : "Ticket costs €12,00");
+
+// Declare a variable named age. If age is below 16, print "serve butter beer". Otherwise print "serve beer".
+
+let age = 27;
+
+console.log(age < 16 ? "serve butter beer" : "serve beer");
+
+// Check if two given integer values are in the range 50 to 99 (this includes both 50 and 99). Print true if either of them are in the range.
+
