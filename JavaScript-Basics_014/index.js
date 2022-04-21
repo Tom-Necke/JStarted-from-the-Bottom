f25f4a4f4487f23092b1e77eee8708de5293fed3
Tom <tom.necke@dci-student.de>
2022-04-21 10:57:08 +0200

    1

---
+++ JavaScript-Basics_014/index.js
@@ -0,0 +1,14 @@
+// Counting Letters. Create a function that counts the number of occurrences of each letter in a string. Return an object with key pair values of letters and the number of occurrences for each letter.
+// Example:
+// countLetters("tree") ➞ {t: 1, r: 1, e: 2}
+
+const countLetters = (str) => {
+    let result = {};
+    for (let i = 0; i < str.length; i++) {
+      result[str[i]] = result[str[i]] + 1 || 1;
+    }
+    return console.log(result);
+};
+  
+countLetters("tree");
+countLetters("Hallo");
