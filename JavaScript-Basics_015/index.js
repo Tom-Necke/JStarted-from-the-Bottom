c734b6e9b8f259fb91d48a76ddc852fbd9bfa7ad
Tom <tom.necke@dci-student.de>
2022-04-26 09:48:30 +0200

    first done

--- JavaScript-Basics_015/index.js
+++ JavaScript-Basics_015/index.js
@@ -26,13 +26,7 @@ for (let i = 0; i < recipe.ingredients.length; i++) {
 // Create an array of objects, where each object describes a book and has properties for the title (a string), author (a string), and alreadyRead (a boolean indicating if you read it yet).
 // Iterate through the array of books. For each book, log the book title and book author like so: "The Hobbit by J.R.R. Tolkien".
 // Now use an if/else statement to change the output depending on whether you read it yet or not. If you read it, log a string like 'You already read "The Hobbit" by J.R.R. Tolkien', and if not, log a string like 'You still need to read "The Lord of the Rings" by J.R.R. Tolkien.'
-// Get Names. Create a function that takes an array of objects containing students' names e.g. {name: "John"}, and returns an array of just student names.
-// Example:
-// getNames([{ name: "Jane" },
-//        { name: "Jack" },
-//        { name: "John" }
-//    ])
-// ➞ ["Jane", "Jack", "John"]
+
 
 const books = [
     {title: "The Hobbit",
@@ -53,3 +47,27 @@ const books = [
       console.log("You still need to read " + bookInfo);
     }
   }
+
+
+// Get Names. Create a function that takes an array of objects containing students' names e.g. {name: "John"}, and returns an array of just student names.
+// Example:
+// getNames([{ name: "Jane" },
+//        { name: "Jack" },
+//        { name: "John" }
+//    ])
+// ➞ ["Jane", "Jack", "John"]
+
+
+const getNames = (arr) => {
+    let result = []
+    for (let i = 0; i < arr.length; i++){
+        result.push(arr[i].name)
+    }
+    return console.log(result);
+}
+
+getNames([{ name: "Jane" },
+       { name: "Jack" },
+       { name: "John" }
+])
+   
