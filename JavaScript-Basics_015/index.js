2d0434c434628e0df62aa64b7b14575a81864ee2
Tom <tom.necke@dci-student.de>
2022-04-26 15:02:33 +0200

    +

--- JavaScript-Basics_015/index.js
+++ JavaScript-Basics_015/index.js
@@ -21,6 +21,21 @@ for (let i = 0; i < recipe.ingredients.length; i++) {
     console.log(recipe.ingredients[i]);
 }
 
+// opt 2
+
+const recipe2 = {
+  name: "Mole",
+  serves: 2,
+  ingredients: ["cinnamon", "cumin", "cocoa"]
+}
+
+console.log(recipe2.name);
+console.log(`Serves: ${recipe2.serves}`);
+console.log(`Ingredients:`);
+for (const element of recipe2.ingredients) {
+  console.log(element);
+}
+
 // The Reading List
 // Keep track of which books you read and which books you want to read!
 // Create an array of objects, where each object describes a book and has properties for the title (a string), author (a string), and alreadyRead (a boolean indicating if you read it yet).
@@ -48,6 +63,24 @@ const books = [
     }
   }
 
+// opt 2
+
+  const books2 = [
+    {
+      title: "book1",
+      author: "author1",
+      read: true
+    },
+    {
+      title: "book2",
+      author: "author2",
+      read: false
+    }
+  ]
+  
+  for (const book of books2) {
+    console.log(`${book.title} by ${book.author}`);
+  }
 
 // Get Names. Create a function that takes an array of objects containing students' names e.g. {name: "John"}, and returns an array of just student names.
 // Example:
@@ -70,4 +103,33 @@ getNames([{ name: "Jane" },
        { name: "Jack" },
        { name: "John" }
 ])
-   
+
+// Create a function that returns an object has following output, try this one in Advance array methods :muskel::hautton-2:
+// Examples:
+// ABC -> { A: 1, B: 1, C: 1 } QQQ -> { Q: 3}
+
+const countLetter = (str) => {
+    const arr = str.split("");
+    return arr.reduce((acc, cur) => {
+      acc[cur] = acc[cur] ? acc[cur] +1 : 1;
+      return acc;
+    }, {});
+  };
+console.log(countLetter("ichbineinsehrlangeswortlol"));
+
+// Scrabble. Write a program that, given an array of scrabble tiles, counts the maximum score that a player can earn from the tiles in their hand.
+// Example:
+// [ { tile: "N", score: 1 }, { tile: "K", score: 5 }, { tile: "Z", score: 10 }, { tile: "X", score: 8 }, { tile: "D", score: 2 }, { tile: "A", score: 1 }, { tile: "E", score: 1 } ]
+
+const scrabble = [
+    { tile: "N", score: 1 },
+    { tile: "K", score: 5 },
+    { tile: "Z", score: 10 },
+    { tile: "X", score: 8 },
+    { tile: "D", score: 2 },
+    { tile: "A", score: 1 },
+    { tile: "E", score: 1 },
+];
+  
+const sumOfScore = (arr) => arr.reduce((acc, cur) => acc + cur.score, 0);
+console.log(sumOfScore(scrabble));
